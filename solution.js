d53a40d060806f98f7052456c21df9c6ca065322
Sumi-Akter <sumi19202103205@gmail.com>
2022-08-10 12:37:09 +0600

    second commit

--- solution.js
+++ solution.js
@@ -315,6 +315,12 @@ let c={ name: 'mobile',
 
 
 // Area of Circle
+function areaOfCircle(r){
+    const pi=3.1416;
+    const area=pi*r*r;
+   return area.toFixed(2);
+}
+console.log(areaOfCircle(4));
 
 
 
